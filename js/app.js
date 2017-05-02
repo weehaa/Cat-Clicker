e7b9028654a43995e184251da7cc5c8b359951b1
weehaa <vzg1000@mail.ru>
2017-05-02 22:38:23 +0400

    refactor with catdata as an argument for a Cat model

--- js/app.js
+++ js/app.js
@@ -1,23 +1,28 @@
-var Cat = function () {
-    this.nicknames = ko.observableArray(['wire eater', 'slippers theft']);
-    this.clickCount = ko.observable(0);
+var Cat = function (catData) {
+    this.nicknames = ko.observableArray(catData.nicknames);
+    this.clickCount = ko.observable(catData.clicks);
     // this.catLevel = ko.observable('Newborn');
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496');
+    this.name = ko.observable(catData.name);
+    this.imgSrc = ko.observable(catData.imgSrc);
 
     this.catLevel = ko.computed(function() {
-        if (this.clickCount() > 20) {
-            return 'Youngster';
-        } else if (this.clickCount() > 10) {
+        if (this.clickCount() < 10) {
+            return 'Newborn';
+        } else if (this.clickCount() < 20) {
             return 'Infant';
         } else {
-            return 'Newborn';
+            return 'Youngster';
         };
     }, this);
 }
 
 var ViewModel = function() {
-    this.currentCat = ko.observable( new Cat() );
+    this.currentCat = ko.observable( new Cat({
+        nicknames: ['wire eater', 'slippers theft'],
+        clicks: 0,
+        name: 'Tabby',
+        imgSrc: 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496'
+    }) );
 
     this.iCounter = function() {
         this.clickCount(this.clickCount() + 1);
