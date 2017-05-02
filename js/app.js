5aadae99932c12a05f8f6969bbba90a00c89d031
weehaa <vzg1000@mail.ru>
2017-05-02 23:31:18 +0400

    refactor with initial cat data outside the ViewModel

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
+var initialCats = [
+        {
+        name: 'Poplinre',
+        imgSrc: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+        clicks: 0,
+        nicknames: ['wire eater', 'slippers theft']
+        },
+        {
+        name: 'Tiger',
+        imgSrc: 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
+        clicks: 0,
+        nicknames: ['Shmiger', 'besider']
+        },
+        {
+        name: "Chewie",
+        imgSrc: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
+        clicks: 0,
+        nicknames: ['Tewie', 'Bewie']
+    }];
+
 var Cat = function (catData) {
     this.nicknames = ko.observableArray(catData.nicknames);
     this.clickCount = ko.observable(catData.clicks);
-    // this.catLevel = ko.observable('Newborn');
     this.name = ko.observable(catData.name);
     this.imgSrc = ko.observable(catData.imgSrc);
 
@@ -17,14 +36,23 @@ var Cat = function (catData) {
 }
 
 var ViewModel = function() {
-    this.currentCat = ko.observable( new Cat({
-        nicknames: ['wire eater', 'slippers theft'],
-        clicks: 0,
-        name: 'Tabby',
-        imgSrc: 'https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496'
-    }) );
+    // we need this in order not to be confused about binding content
+    var self = this;
+    // create observable array from initial data
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function (catItem) {
+        self.catList.push( new Cat(catItem) );
+    });
 
+    // define default current cat
+    this.currentCat = ko.observable(this.catList()[0]);
+    // set a current cat on click
+    this.catClick = function () {
+        self.currentCat(this);
+    };
+    // iCounter is in the currentCat() binding context !!!
     this.iCounter = function() {
+        // 'this' here is a currentCat()!
         this.clickCount(this.clickCount() + 1);
     };
 
