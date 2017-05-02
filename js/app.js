707bab4d79a41f262a9e8ea5af3ea748945d9ad7
weehaa <vzg1000@mail.ru>
2017-05-02 20:12:46 +0400

    separate Cat from ViewModel

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-var ViewModel = function() {
+var Cat = function () {
+    this.nicknames = ko.observableArray(['wire eater', 'slippers theft']);
     this.clickCount = ko.observable(0);
     // this.catLevel = ko.observable('Newborn');
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496');
-
-    this.iCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
 
     this.catLevel = ko.computed(function() {
         if (this.clickCount() > 20) {
@@ -16,7 +13,15 @@ var ViewModel = function() {
         } else {
             return 'Newborn';
         };
-
     }, this);
+}
+
+var ViewModel = function() {
+    this.currentCat = ko.observable( new Cat() );
+
+    this.iCounter = function() {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
+
 }
 ko.applyBindings(new ViewModel());
